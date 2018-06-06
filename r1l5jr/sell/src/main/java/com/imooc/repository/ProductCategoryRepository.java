f1782d4432b830c66dbb342f015717470676ec89
zhangey <251647363@qq.com>
2018-06-06 14:48:39 +0800

    hello aaa

--- r1l5jr/sell/src/main/java/com/imooc/repository/ProductCategoryRepository.java
+++ r1l5jr/sell/src/main/java/com/imooc/repository/ProductCategoryRepository.java
@@ -11,5 +11,6 @@ import java.util.List;
  */
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Integer> {
 
+
     List<ProductCategory> findByCategoryTypeIn(List<Integer> categoryTypeList);
 }
