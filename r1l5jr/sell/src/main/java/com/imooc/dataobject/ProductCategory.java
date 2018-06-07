dfab4d0b41eb86226cce70dc9a2a5e88666da553
zhangey <251647363@qq.com>
2018-06-07 23:00:56 +0800

    hello aaa

--- r1l5jr/sell/src/main/java/com/imooc/dataobject/ProductCategory.java
+++ r1l5jr/sell/src/main/java/com/imooc/dataobject/ProductCategory.java
@@ -17,15 +17,21 @@ import javax.persistence.Id;
 @Data
 public class ProductCategory {
 
-    /** 类目id. */
+    /**
+     * 类目id.
+     */
     @Id
     @GeneratedValue
     private Integer categoryId;
 
-    /** 类目名字. */
+    /**
+     * 类目名字.
+     */
     private String categoryName;
 
-    /** 类目编号. */
+    /**
+     * 类目编号.
+     */
     private Integer categoryType;
 
     public ProductCategory() {
